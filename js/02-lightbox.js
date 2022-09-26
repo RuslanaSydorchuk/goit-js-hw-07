02d8ae85e64f4f6ae0fd981bbfca27bbb928f4cc
RuslanaSydorchuk <104679795+RuslanaSydorchuk@users.noreply.github.com>
2022-09-26 22:08:13 +0300

    Update 02-lightbox.js
    виконане 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection => {
+    collection.map(({ preview, original, description }) => {
+        const addElements = 
+        `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}"/>
+    </a>`;
+        galaryDiv.insertAdjacentHTML('beforeend', addElements);
+    })
+}
+
+createGalery(galleryItems)
+
+
+const onClick = evt => {
+    evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+}
+galaryDiv.addEventListener('click', onClick);
